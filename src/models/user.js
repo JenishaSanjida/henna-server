255078b2485dcdc13eedb5e7355da2762e0ff0bd
shahnawaz-pabon <s.pabon93@gmail.com>
2023-04-08 22:08:22 +0600

    required models created with mongodb configurations

---
+++ src/models/user.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+
+const scheduleSchema = new mongoose.Schema({
+  day: {
+    type: String,
+    enum: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
+    required: true,
+  },
+  startTime: {
+    type: String,
+    required: true,
+  },
+  endTime: {
+    type: String,
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["customer", "designer"],
+    required: true,
+    default: "customer",
+  },
+  address: {
+    type: String,
+  },
+  territory: {
+    type: String,
+  },
+  schedule: {
+    type: [scheduleSchema],
+    required: true,
+  },
+  portfolio: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Portfolio",
+  },
+  ratings: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Rating",
+    },
+  ],
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
