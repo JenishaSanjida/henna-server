2198cb7eb832aee2b3983d69c6f021562edeb56e
JenishaSanjida <srjenisha13@gmail.com>
2023-04-08 22:27:05 +0600

    server initialized with basic route

---
+++ src/server.js
@@ -0,0 +1,34 @@
+// Import dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./config/db");
+
+// Import API routes
+// const authRoutes = require('./routes/auth');
+// const customerRoutes = require('./routes/customers');
+
+// Set up the server
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Use middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Use API routes
+// app.use('/api/auth', authRoutes);
+// app.use('/api/customers', customerRoutes);
+
+app.get("/users", async (req, res) => {
+  const users = await db.User.find();
+  res.send(users);
+});
+
+app.get("/", async (req, res) => {
+  res.send({ message: "successful" });
+});
+
+// Start the server
+app.listen(port, () => console.log(`Server started on port ${port}`));
