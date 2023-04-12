5b06ae82a2f707a1faeac87f0d20a311951601c3
JenishaSanjida <srjenisha13@gmail.com>
2023-04-13 00:25:12 +0600

    file upload api implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
-// Import dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./config/db");
-
 // Import API routes
 // const authRoutes = require('./routes/auth');
+const userRouter = require("./routes/user");
 // const customerRoutes = require('./routes/customers');
 
 // Set up the server
@@ -23,14 +22,14 @@ app.use("/api", userRouter);
 // app.use('/api/auth', authRoutes);
 // app.use('/api/customers', customerRoutes);
 
-app.get("/users", async (req, res) => {
-  const users = await db.User.find();
-  res.send(users);
-});
+// app.get("/users", async (req, res) => {
+//   const users = await db.User.find();
+//   res.send(users);
+// });
 
-app.get("/", async (req, res) => {
-  res.send({ message: "successful" });
-});
+// app.get("/", async (req, res) => {
+//   res.send({ message: "successful" });
+// });
 
 // Start the server
 app.listen(port, () => console.log(`Server started on port ${port}`));
