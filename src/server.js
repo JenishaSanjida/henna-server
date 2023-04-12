f83a6bfdd6bb006a53296b86c7c0d4c7d6cdebe0
JenishaSanjida <srjenisha13@gmail.com>
2023-04-12 23:56:43 +0600

    path, multer installed with upload files configuration

--- src/server.js
+++ src/server.js
@@ -17,7 +17,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use("/uploads", express.static("/app/uploads")); // uploaded files
 // Use API routes
+app.use("/api", userRouter);
 // app.use('/api/auth', authRoutes);
 // app.use('/api/customers', customerRoutes);
 
